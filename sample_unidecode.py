29b7aba7278a444ee89b8c876c5be51478e26dd8
= <=>
2016-05-30 08:42:19 -0700

    Remarked Python scripts; added summary csv as streetz.db data source

--- sample_unidecode.py
+++ sample_unidecode.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# sample_unidecode.py cleans out non-utf8 characters and replaces them with utf8. PostGIS is
+# sensitive; this step is extremely helpful. More info on the Python modules used are in the README.
+
 from unidecode import unidecode
 
 def remove_non_ascii(text):
     return unidecode(unicode(text, encoding = "utf-8"))
+
+# Each line of your infile_csv contains street names and coordinates from OpenStreeMap.
+# The output is converted to a shapefile or you can modify this script for parsing
+# direcly into PostGIS.
 
 infile_csv = open('infile.csv', 'r')
 outfile_csv = open('outfile.csv', 'w')
--- streetzip.py
+++ streetzip.py
@@ -1,21 +1,29 @@
+# streetzip.py queries the streetz.db sqlite database and appends a list of zipcodes
+# and street names into streetlist.txt. The streetz.db file contains an indexed list
+# of street names and zipcodes extracted from OpenStreetMap data for California.
+# Methods and data sources are in the REAME.
+
 import webbrowser
 import sqlite3
 
+# Ask for user input in the CMD Console
 print "This program gives you a list of streets in a given California Zip Code\n"
 user_zip = raw_input( "Enter Zip Code:  " )
 user_street = raw_input( "Enter Part of Street Name:  " )
 print ""
 
-streetlist_file = open('streetlist.txt', 'w')
+# Open streetlist.txt
+streetlist_file = open("streetlist.txt", "w")
+streetlist_file.write("ZIP   STREET")
 
+# parse a query search string qsearch and iterate database output into streetlist.txt
 c = sqlite3.connect('streetz.db')
-    
-# parse a query search string qsearch
 qsrch = "SELECT [st_name], [zip] FROM streetz WHERE zip like '%" + user_zip + "%' AND st_name like '%" + user_street + "%' ORDER BY [st_name]"
-
 for row in c.execute(qsrch):
   streetlist_file.write(str(row[1]) + " " + str(row[0]) + "\n")
 
+
 streetlist_file.close()
 
-webbrowser.open('streetlist.txt')
+# Display streetlist.txt
+webbrowser.open("streetlist.txt")
