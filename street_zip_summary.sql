b33b834a38f9bb00ee559ee39758899273cc3c45
= <=>
2016-07-07 22:05:32 -0700

    Minor updates automation process

--- street_zip_summary.sql
+++ street_zip_summary.sql
@@ -1,11 +1,11 @@
-SELECT 
-osm_pts.name AS st_name,
-COUNT(osm_pts.name) as st_count,
+﻿SELECT
+roads.name st_name,
 zip_codes.zip_code AS zip,
-zip_codes.name AS community
+zip_codes.name AS community,
+COUNT(roads.name) as st_count
 FROM zip_codes
-JOIN osm_pts
-ON ST_Contains(zip_codes.geom, osm_pts.geom)
-GROUP BY osm_pts.name, zip_codes.zip_code, zip_codes.name
-ORDER BY osm_pts.name
+join roads
+ON ST_intersects(zip_codes.geom, roads.geom)
+GROUP BY roads.name, zip_codes.zip_code, zip_codes.name
+ORDER BY roads.name
 ;
