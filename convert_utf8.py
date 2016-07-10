ca912aa5a52173a1f21810ccb6ae29bab17765c2
= <=>
2016-07-10 15:01:47 -0700

    Updated convert_utf8.py to remove escape chars

--- convert_utf8.py
+++ convert_utf8.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# convert_utf8.py inputs roads_wkt.csv and outputs roads_wkt_utf8.csv, containing only wkt and name fields.
-# It also replaces any special charcters with utf-8 characters usng the unidecode module. Note the unidecode
-# module is not included with standard Python; see https://pypi.python.org/pypi/Unidecode
+# convert_utf8.py inputs roads_wkt.csv and outputs roads_wkt_utf8.sql. This process
+# 1. Extracts only records that have non null street name values
+# 2. Of the above, only the WKT and name fields are extracted (see https://pypi.python.org/pypi/Unidecode)
+# 3. Transliterates special characters with UTF-8 characters
+# 4. Removes other characters unneded and incompatible with sqlite
 
 import csv
 from unidecode import unidecode
@@ -21,7 +23,18 @@ with open('roads_wkt.csv', 'rb') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
     for row in reader:
         if row['name'] > '':
-            new_row = "INSERT INTO roads(name, geom) VALUES ('" + remove_non_ascii(row['name']) + "',ST_GeomFromText('" + row['WKT'] + "',4326));\n"
+            name = remove_non_ascii(row['name'])
+            name = name.replace('"','')
+            name = name.replace("'","")
+            name = name.replace('\\','')
+            name = name.replace('\a','a')
+            name = name.replace('\b','b')
+            name = name.replace('\f','f')
+            name = name.replace('\n','n')
+            name = name.replace('\r','r')
+            name = name.replace('\t','t')
+            name = name.replace('\v','v')
+            new_row = "INSERT INTO roads(name, geom) VALUES ('" + name + "',ST_GeomFromText('" + row['WKT'] + "',4326));\n"
             outfile_csv.write(new_row)
 
 infile_csv.close()
